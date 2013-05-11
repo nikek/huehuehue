557f2674ff80329da55135f0c2b55d5e5a934bc1
Daniel Andersson <Danne@Danmac.local>
2013-05-11 15:51:03 +0200

    added function to change three colors at once

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ app.get('/changecolor/:color', function(req, res){
 	res.send(req.params.color);
 });
 
+app.get('/change3colors/:startindex', function(req, res){
+	setThreeColors(req.params.startindex, res);
+	
+});
+
+
 app.get('/instagram/:tag', function(req, res){
 	getLatestInstagramPhotoByTag(req.params.tag, res);
 });
@@ -55,6 +61,15 @@ var hej = function(){
 	return json;
 };
 
+var setThreeColors = function(startindex, res){
+	var colors = ['ff0000', '00ff00','0000ff'];
+	for (var i=0; i < 3; i++) {
+		color = colors[(i+startindex)%3];
+		var lampstate = hue.setColor(i+1, color);
+		lampstate += hue.setBrightness(i+1, Math.floor(Math.random()*250));
+	};
+	res.send(lampstate);
+}
 
 var getLatestInstagramPhotoByTag = function(tag, res) {
 	var instagramUrl = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?client_id=" + client_id + "&access_token=" + access_token + "&count=1";
@@ -69,6 +84,7 @@ var getLatestInstagramPhotoByTag = function(tag, res) {
 		}
 	});
 };
+
 
 
 /* Start server */
