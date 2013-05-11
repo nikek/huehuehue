977639e58e67cccdab1057911e92ad5b5468980b
Niklas Ek <ek.niklas@gmail.com>
2013-05-11 10:44:08 +0200

    node with express and example code

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+
+app.get('/yo', function(req, res){
+	var responseText = hej();
+  res.send(responseText);
+});
+
+
+var hej = function(){
+	return "yo man";
+};
+
+app.listen(3000);
+console.log('Listening on port 3000');
